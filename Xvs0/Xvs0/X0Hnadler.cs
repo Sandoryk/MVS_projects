d18192215b63948eb9b8062467e93982eb969a6c
Sandoryk <sandorykfa@gmail.com>
2015-11-16 09:38:56 +0200

    new

---
+++ Xvs0/Xvs0/X0Hnadler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xvs0
+{
+    class X0Hnadler
+    {
+        public int player = 0;
+        public string[] playerhist = new string[2];
+        public bool finishedgamef = false;
+
+        public X0Hnadler()
+        {
+            this.InitGame();
+        }
+
+        public void InitGame()
+        {
+            player = 0;
+            finishedgamef = false;
+            for (int i = 0; i < playerhist.Length; i++)
+            {
+                playerhist[i] = "";
+            }
+        }
+
+        public bool CheckForVictory(string hist)
+        {
+            bool res = false;
+
+            if (hist.Length > 2)
+            {
+                if (hist.Contains("1") && hist.Contains("2") && hist.Contains("3"))
+                    res = true;
+                else if (hist.Contains("4") && hist.Contains("5") && hist.Contains("6"))
+                    res = true;
+                else if (hist.Contains("7") && hist.Contains("8") && hist.Contains("9"))
+                    res = true;
+                else if (hist.Contains("1") && hist.Contains("4") && hist.Contains("7"))
+                    res = true;
+                else if (hist.Contains("2") && hist.Contains("5") && hist.Contains("8"))
+                    res = true;
+                else if (hist.Contains("3") && hist.Contains("6") && hist.Contains("9"))
+                    res = true;
+                else if (hist.Contains("1") && hist.Contains("5") && hist.Contains("9"))
+                    res = true;
+                else if (hist.Contains("3") && hist.Contains("5") && hist.Contains("7"))
+                    res = true;
+            }
+            if (res)
+                finishedgamef = true;
+
+            return res;
+        }
+
+        public void ChangePlayer()
+        {
+            if (player == 0)
+                player = 1;
+            else
+                player = 0;
+        }
+
+        public string GetPlayerName()
+        {
+            string name;
+            if (player == 0)
+                name = "Player 1";
+            else
+                name = "Player 2";
+            return name;
+        }
+    }
+}
---
+++ Xvs0/Xvs0/X0window.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Xvs0
+{
+    partial class X0window
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cell_1 = new System.Windows.Forms.Button();
+            this.cell_2 = new System.Windows.Forms.Button();
+            this.cell_3 = new System.Windows.Forms.Button();
+            this.cell_6 = new System.Windows.Forms.Button();
+            this.cell_5 = new System.Windows.Forms.Button();
+            this.cell_4 = new System.Windows.Forms.Button();
+            this.cell_9 = new System.Windows.Forms.Button();
+            this.cell_8 = new System.Windows.Forms.Button();
+            this.cell_7 = new System.Windows.Forms.Button();
+            this.playerturn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Bgwker = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cell_1
+            // 
+            this.cell_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_1.Location = new System.Drawing.Point(35, 88);
+            this.cell_1.Name = "cell_1";
+            this.cell_1.Size = new System.Drawing.Size(80, 80);
+            this.cell_1.TabIndex = 26;
+            this.cell_1.UseVisualStyleBackColor = true;
+            this.cell_1.Click += new System.EventHandler(this.cell_1_Click);
+            // 
+            // cell_2
+            // 
+            this.cell_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_2.Location = new System.Drawing.Point(121, 88);
+            this.cell_2.Name = "cell_2";
+            this.cell_2.Size = new System.Drawing.Size(80, 80);
+            this.cell_2.TabIndex = 25;
+            this.cell_2.UseVisualStyleBackColor = true;
+            this.cell_2.Click += new System.EventHandler(this.cell_2_Click);
+            // 
+            // cell_3
+            // 
+            this.cell_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_3.Location = new System.Drawing.Point(207, 88);
+            this.cell_3.Name = "cell_3";
+            this.cell_3.Size = new System.Drawing.Size(80, 80);
+            this.cell_3.TabIndex = 24;
+            this.cell_3.UseVisualStyleBackColor = true;
+            this.cell_3.Click += new System.EventHandler(this.cell_3_Click);
+            // 
+            // cell_6
+            // 
+            this.cell_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_6.Location = new System.Drawing.Point(207, 174);
+            this.cell_6.Name = "cell_6";
+            this.cell_6.Size = new System.Drawing.Size(80, 80);
+            this.cell_6.TabIndex = 21;
+            this.cell_6.UseVisualStyleBackColor = true;
+            this.cell_6.Click += new System.EventHandler(this.cell_6_Click);
+            // 
+            // cell_5
+            // 
+            this.cell_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_5.Location = new System.Drawing.Point(121, 174);
+            this.cell_5.Name = "cell_5";
+            this.cell_5.Size = new System.Drawing.Size(80, 80);
+            this.cell_5.TabIndex = 22;
+            this.cell_5.UseVisualStyleBackColor = true;
+            this.cell_5.Click += new System.EventHandler(this.cell_5_Click);
+            // 
+            // cell_4
+            // 
+            this.cell_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_4.Location = new System.Drawing.Point(35, 174);
+            this.cell_4.Name = "cell_4";
+            this.cell_4.Size = new System.Drawing.Size(80, 80);
+            this.cell_4.TabIndex = 23;
+            this.cell_4.UseVisualStyleBackColor = true;
+            this.cell_4.Click += new System.EventHandler(this.cell_4_Click);
+            // 
+            // cell_9
+            // 
+            this.cell_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_9.Location = new System.Drawing.Point(207, 260);
+            this.cell_9.Name = "cell_9";
+            this.cell_9.Size = new System.Drawing.Size(80, 80);
+            this.cell_9.TabIndex = 18;
+            this.cell_9.UseVisualStyleBackColor = true;
+            this.cell_9.Click += new System.EventHandler(this.cell_9_Click);
+            // 
+            // cell_8
+            // 
+            this.cell_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_8.Location = new System.Drawing.Point(121, 260);
+            this.cell_8.Name = "cell_8";
+            this.cell_8.Size = new System.Drawing.Size(80, 80);
+            this.cell_8.TabIndex = 19;
+            this.cell_8.UseVisualStyleBackColor = true;
+            this.cell_8.Click += new System.EventHandler(this.cell_8_Click);
+            // 
+            // cell_7
+            // 
+            this.cell_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cell_7.Location = new System.Drawing.Point(35, 260);
+            this.cell_7.Name = "cell_7";
+            this.cell_7.Size = new System.Drawing.Size(80, 80);
+            this.cell_7.TabIndex = 20;
+            this.cell_7.UseVisualStyleBackColor = true;
+            this.cell_7.Click += new System.EventHandler(this.cell_7_Click);
+            // 
+            // playerturn
+            // 
+            this.playerturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.playerturn.Location = new System.Drawing.Point(128, 365);
+            this.playerturn.Name = "playerturn";
+            this.playerturn.ReadOnly = true;
+            this.playerturn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.playerturn.Size = new System.Drawing.Size(126, 26);
+            this.playerturn.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(36, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Turn of the";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 24);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "New game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Background worker";
+            // 
+            // Bgwker
+            // 
+            this.Bgwker.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Bgwker.Location = new System.Drawing.Point(248, 12);
+            this.Bgwker.Multiline = true;
+            this.Bgwker.Name = "Bgwker";
+            this.Bgwker.ReadOnly = true;
+            this.Bgwker.Size = new System.Drawing.Size(39, 59);
+            this.Bgwker.TabIndex = 1;
+            // 
+            // X0window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 444);
+            this.Controls.Add(this.Bgwker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playerturn);
+            this.Controls.Add(this.cell_9);
+            this.Controls.Add(this.cell_8);
+            this.Controls.Add(this.cell_7);
+            this.Controls.Add(this.cell_6);
+            this.Controls.Add(this.cell_5);
+            this.Controls.Add(this.cell_4);
+            this.Controls.Add(this.cell_3);
+            this.Controls.Add(this.cell_2);
+            this.Controls.Add(this.cell_1);
+            this.Name = "X0window";
+            this.Text = "Main window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cell_1;
+        private System.Windows.Forms.Button cell_2;
+        private System.Windows.Forms.Button cell_3;
+        private System.Windows.Forms.Button cell_6;
+        private System.Windows.Forms.Button cell_5;
+        private System.Windows.Forms.Button cell_4;
+        private System.Windows.Forms.Button cell_9;
+        private System.Windows.Forms.Button cell_8;
+        private System.Windows.Forms.Button cell_7;
+        private System.Windows.Forms.TextBox playerturn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Bgwker;
+    }
+}
+
