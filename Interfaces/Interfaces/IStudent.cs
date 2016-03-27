9e7907aec11ed4cc8113f3a9d994f0aa7aae27a8
Sandoryk <sandorykfa@gmail.com>
2016-03-27 22:12:47 +0300

    !

---
+++ Interfaces/Interfaces/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IStudent
+    {
+        int Course { get; set; }
+        string Faculty { get; set; }
+        DateTime CreationDT { get; }
+        /*void GetCourse();
+        void GetFaculty();*/
+        void FullInfoOutput();
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonInfo perInfo = new PersonInfo();
+            IPerson person = perInfo;
+            person.FullInfoOutput();
+            IStudent student = perInfo;
+            student.FullInfoOutput();
+        }
+    }
+}
