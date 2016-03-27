3d911ff5921625eb7d77f3218789ad53eb9b344a
Sandoryk <sandorykfa@gmail.com>
2016-03-27 22:06:49 +0300

    !

---
+++ Interfaces/Interfaces/IPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IPerson
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        DateTime DateOfBirth { get; set; }
+        DateTime CreationDT { get; }
+        /*void GetName();
+        void GetAge();
+        void GetDateOfBirth();*/
+
+        void FullInfoOutput();
+
+    }
+}
