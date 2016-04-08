c308e3dca5523294ecc02b922806eea186d089d2
Sandoryk <sandorykfa@gmail.com>
2016-04-08 22:37:49 +0300

    Topic_5_Task_1

---
+++ Topic_5_Task_1/Topic_5_Task_1/FileOperator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Topic_5_Task_1
+{
+    class FileOperator
+    {
+        long maxlen;
+
+        public delegate int OverStackHandler(object sender,EventArgs e);
+
+        public OverStackHandler OnOverStack;
+        public FileOperator(long maxlen)
+        {
+            this.maxlen = maxlen;
+        }
+        public void WriteDataInFile(StreamWriter filewriter)
+        {
+            int choise;
+            bool choiseIsMade = false;
+
+            Console.WriteLine("Start writing");
+            for (int i = 0; i < 1500; i++)
+            {
+                filewriter.WriteLine("Recorded string #" + (i+1));
+                if(i%100==0)
+                    Console.WriteLine("Length " + filewriter.BaseStream.Length);
+                if (choiseIsMade == false && filewriter.BaseStream.Length > maxlen)
+                {
+                    choiseIsMade = true;
+                    choise = OnOverStack(this, new EventArgs());
+                    if (choise>0)
+                        break;
+                    else if (choise==0)
+                    {
+                        FileStream g = (FileStream)filewriter.BaseStream;
+                        g.SetLength(0);
+                        break;
+                    }
+                }  
+            }
+        }
+        public void ReadDataInFile(StreamReader filereader)
+        {
+            string line = "";
+            Console.WriteLine("Start reading");
+            line = filereader.ReadLine();
+            while (String.IsNullOrEmpty(line)==false)
+            {
+                Console.WriteLine(line);
+                line = filereader.ReadLine();
+            }
+                
+        }
+    }
+}
---
+++ Topic_5_Task_1/Topic_5_Task_1/ProgramRun.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Topic_5_Task_1
+{
+    class ProgramRun
+    {
+        public FileStream file;
+        public StreamWriter fileoutput;
+        public StreamReader fileinput;
+        public string filename = "BufferFIle.txt";
+        static void Main(string[] args)
+        {
+            ProgramRun pr = new ProgramRun();
+            FileOperator custfwr = new FileOperator(20000);
+            custfwr.OnOverStack = pr.OverStackMessageMethod;
+
+            try
+            {
+                pr.file = new FileStream(pr.filename, FileMode.Create, FileAccess.ReadWrite);
+                using (pr.fileoutput = new StreamWriter(pr.file))
+                {
+                    
+                    custfwr.WriteDataInFile(pr.fileoutput);
+                    //fileoutput.WriteLine("----------------------------------");
+                    //fileoutput.Flush();
+                }
+                pr.file = new FileStream(pr.filename, FileMode.Open, FileAccess.ReadWrite);
+                using (pr.fileinput = new StreamReader(pr.file))
+                {
+                    custfwr.ReadDataInFile(pr.fileinput);
+                }
+            }
+            catch (IOException Error)
+            {
+                Console.WriteLine(Error.Message);
+            }
+        }
+        public int OverStackMessageMethod(object sender,EventArgs args)
+        {
+            int result = 0;
+            MessageBoxManager.Yes = "Cancel writing";
+            MessageBoxManager.No = "Finish writing";
+            MessageBoxManager.Register();
+            DialogResult dialogResult = MessageBox.Show("Choose action", "OverStack happened", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                result = 1;
+            }
+            return result;
+        }
+    }
+}
