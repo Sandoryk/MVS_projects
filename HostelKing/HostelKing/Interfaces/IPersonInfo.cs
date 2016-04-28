4746977d27d744be9995f20c689486f0d09ff4a7
Sandoryk <sandorykfa@gmail.com>
2016-04-28 21:57:26 +0300

    !

---
+++ HostelKing/HostelKing/Interfaces/IPersonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelKing.Interfaces
+{
+    interface IPersonInfo
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateBirth { get; set; }
+        string RoomNumber { get; set; }
+        string Sex { get; set; }
+    }
+}
