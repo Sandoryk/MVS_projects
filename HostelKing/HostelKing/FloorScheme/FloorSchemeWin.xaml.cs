90c08ff19a6a5a076a63ddcaa7bc1d0e706a4197
Sandoryk <sandorykfa@gmail.com>
2016-05-05 14:02:42 +0300

    !

--- HostelKing/HostelKing/FloorScheme/FloorSchemeWin.xaml.cs
+++ HostelKing/HostelKing/FloorScheme/FloorSchemeWin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace HostelKing
 {
@@ -21,7 +24,48 @@ namespace HostelKing
     {
         public FloorSchemeView()
         {
+            StringReader stringReader;
+            XmlReader xmlReader;
+
             InitializeComponent();
+
+            string viewBoxXaml = XamlWriter.Save(this.FlatViewBox);
+            int columnsCount = this.FlatsGrid.ColumnDefinitions.Count;
+            for (int i = 0; i < columnsCount-1; i++)
+            {
+                if (i==3)
+                {
+                    continue;
+                }
+                using (stringReader = new StringReader(viewBoxXaml))
+                {
+                    xmlReader = XmlReader.Create(stringReader);
+                    Viewbox newViewBox = (Viewbox)XamlReader.Load(xmlReader);
+                    this.FlatsGrid.Children.Add(newViewBox);
+                    Grid.SetColumn(newViewBox, i);
+                }
+            }
+            for (int i = 0; i < columnsCount-1; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                using (stringReader = new StringReader(viewBoxXaml))
+                {
+                    xmlReader = XmlReader.Create(stringReader);
+                    Viewbox newViewBox = (Viewbox)XamlReader.Load(xmlReader);
+                    newViewBox.RenderTransformOrigin = new Point(0.5,0.5);
+                    newViewBox.RenderTransform = new RotateTransform(180);
+                    this.FlatsGrid.Children.Add(newViewBox);
+                    Grid.SetColumn(newViewBox, i);
+                    Grid.SetRow(newViewBox, 2);
+                }
+            }
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).Value = Math.Round(e.NewValue,0);
         }
     }
 }
