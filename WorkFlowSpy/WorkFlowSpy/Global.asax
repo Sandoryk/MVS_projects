f80f4d7ea8aa98e283581f8294045f7d30aef4b6
Sandoryk <sandorykfa@gmail.com>
2016-09-21 01:10:10 +0300

    !

---
+++ WorkFlowSpy/WorkFlowSpy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkFlowSpy.MvcApplication" Language="C#" %>
---
+++ WorkFlowSpy/WorkFlowSpy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkFlowSpy.Startup))]
+namespace WorkFlowSpy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
