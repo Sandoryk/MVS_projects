d319022e7f246171cb84670d66927de8a1d69dc8
Sandoryk <sandorykfa@gmail.com>
2016-09-21 01:40:04 +0300

    Revert "!"
    This reverts commit f80f4d7ea8aa98e283581f8294045f7d30aef4b6.

--- TestASP/TestASP/Controllers/HomeController.cs
+++ TestASP/TestASP/Controllers/HomeController.cs
@@ -8,16 +8,9 @@ namespace TestASP.Controllers
 {
     public class HomeController : Controller
     {
-        public ContentResult Index()
+        public ActionResult Index()
         {
-            //return View();
-            string browser = HttpContext.Request.Browser.Browser;
-            string user_agent = HttpContext.Request.UserAgent;
-            string url = HttpContext.Request.RawUrl;
-            string ip = HttpContext.Request.UserHostAddress;
-            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
-            return Content("<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
-                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>");
+            return View();
         }
 
         public ActionResult About()
