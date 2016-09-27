bab89456b62b3c80f7ea4243aa92affb671cbdc8
Sandoryk <sandorykfa@gmail.com>
2016-09-27 08:57:08 +0300

    !

--- WorkFlowSpy/WorkFlowSpy/Controllers/HomeController.cs
+++ WorkFlowSpy/WorkFlowSpy/Controllers/HomeController.cs
@@ -20,24 +20,6 @@ namespace WorkFlowSpy.Controllers
 
         public ActionResult Diagram()
         {
-            ViewBag.Message = "Your application description page.";
-
-            /*using (DataSourceHandler dataSource = new DataSourceHandler("WorkFlowDbConnection"))
-            {
-                List<TaskDB> tasks = new List<TaskDB>()
-                {
-                    new TaskDB() { TaskId = 1, Text = "Project #2", StartDate = DateTime.Now.AddHours(-3),
-                        Duration = 18, SortOrder = 10, Progress = 0.4m, ParentId = null, AssignedToId = 1 },
-                    new TaskDB() { TaskId = 2, Text = "Task #1", StartDate = DateTime.Now.AddHours(-2),
-                        Duration = 8, SortOrder = 10, Progress = 0.6m, ParentId = 1, AssignedToId = 1 },
-                    new TaskDB() { TaskId = 3, Text = "Task #2", StartDate = DateTime.Now.AddHours(-1),
-                        Duration = 8, SortOrder = 20, Progress = 0.6m, ParentId = 1, AssignedToId = 2 }
-                };
-
-                tasks.ForEach(s => dataSource.Tasks.Create(s));
-                dataSource.Save();
-            }*/
-
             DiagramAdapter DAdapter = new DiagramAdapter();
             JsonResult json = new JsonResult();
 
